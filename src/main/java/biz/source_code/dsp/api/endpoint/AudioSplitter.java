3f2a5d05ec7299358fea7290e9dbecd4b5ed3dc8
juan890202 <jcflorezr@gmail.com>
2017-04-02 18:54:08 -0500

    [audio-splitter] adding support to manage the success and failed responses

--- src/main/java/biz/source_code/dsp/api/endpoint/AudioSplitter.java
+++ src/main/java/biz/source_code/dsp/api/endpoint/AudioSplitter.java
@@ -1,6 +1,7 @@
 package biz.source_code.dsp.api.endpoint;
 
 import biz.source_code.dsp.api.audioclips.AudioClipsGenerator;
+import biz.source_code.dsp.api.model.AudioSoundZoneInfo;
 import biz.source_code.dsp.api.model.response.AudioSplitterResponse;
 import biz.source_code.dsp.audioclips.AudioClipsGeneratorImpl;
 import biz.source_code.dsp.audiofileinfo.AudioFileInfoService;
@@ -9,9 +10,9 @@ import biz.source_code.dsp.exceptions.BadRequestException;
 import biz.source_code.dsp.exceptions.InternalServerErrorException;
 import biz.source_code.dsp.model.AudioFileInfo;
 import biz.source_code.dsp.model.AudioFileLocation;
-import biz.source_code.dsp.model.GroupAudioSoundZonesInfo;
 import biz.source_code.dsp.model.OutputAudioClipsConfig;
-import biz.source_code.dsp.model.SingleAudioSoundZoneInfo;
+import biz.source_code.dsp.model.response.ErrorResponse;
+import biz.source_code.dsp.model.response.SuccessResponse;
 import biz.source_code.dsp.util.AudioFormatsSupported;
 import org.apache.commons.io.FilenameUtils;
 
@@ -19,7 +20,9 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.stream.Stream;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public abstract class AudioSplitter {
 
@@ -29,41 +32,31 @@ public abstract class AudioSplitter {
     protected abstract AudioSplitterResponse generateAudioClips(AudioFileLocation audioFileLocation);
 
     protected abstract AudioSplitterResponse generateAudioMonoClips(AudioFileLocation audioFileLocation);
-
-    protected AudioSplitterResponse generateAudioClipsByGroup(AudioFileLocation audioFileLocation, AudioFormatsSupported audioFormat, boolean asMono) {
-        boolean generateAudioClipsByGroup = true;
-        return generateAudioClips(audioFileLocation, audioFormat, asMono, generateAudioClipsByGroup);
-    }
     
-    protected AudioSplitterResponse generateSingleAudioClips(AudioFileLocation audioFileLocation, AudioFormatsSupported audioFormat, boolean asMono) {
-        boolean generateAudioClipsByGroup = false;
-        return generateAudioClips(audioFileLocation, audioFormat, asMono, generateAudioClipsByGroup);
-    }
-    
-    private AudioSplitterResponse generateAudioClips(AudioFileLocation audioFileLocation, AudioFormatsSupported audioFormat, boolean asMono, boolean generateAudioClipsByGroup) {
+    public AudioSplitterResponse generateAudioClips(AudioFileLocation audioFileLocation, AudioFormatsSupported audioFormat, boolean asMono, boolean generateAudioClipsByGroup) {
+        ErrorResponse errorResponse;
         try {
             validateAudioFileLocationInfo(audioFileLocation);
             AudioFileInfo audioFileInfo = audioFileInfoService.generateAudioFileInfo(audioFileLocation, generateAudioClipsByGroup);
             OutputAudioClipsConfig outputAudioClipsConfig = audioFileInfo.getOutputAudioClipsConfig(audioFormat, asMono);
-            if (generateAudioClipsByGroup) {
-                Stream<GroupAudioSoundZonesInfo> groupedSoundZonesStream = audioFileInfo.getGroupedAudioFileSoundZones().stream();
-                groupedSoundZonesStream.forEach(groupedSoundZone ->
-                        groupedSoundZone.setAudioClipWritingResult(audioClipsGenerator.generateGroupSoundZonesAudioFile(groupedSoundZone, outputAudioClipsConfig)));
-            } else {
-                Stream<SingleAudioSoundZoneInfo> groupedSoundZonesStream = audioFileInfo.getSingleAudioSoundZones().stream();
-                groupedSoundZonesStream.forEach(soundZone ->
-                        soundZone.setAudioClipWritingResult(audioClipsGenerator.generateSingleSoundZoneAudioFile(soundZone, outputAudioClipsConfig)));
-            }
-        } catch (IOException | BadRequestException e) {
-            System.out.println(new BadRequestException(e.getMessage()));
+            List<? extends AudioSoundZoneInfo> soundZonesInfo =
+                    generateAudioClipsByGroup ? audioFileInfo.getGroupedAudioFileSoundZones() : audioFileInfo.getSingleAudioSoundZones();
+            soundZonesInfo.stream().forEach(groupedSoundZone ->
+                    groupedSoundZone.setAudioClipWritingResult(audioClipsGenerator.generateSoundZoneAudioFile(groupedSoundZone, outputAudioClipsConfig, generateAudioClipsByGroup)));
+            Map<Boolean, Long> soundZonesGenerationResult = soundZonesInfo.stream()
+                    .collect(Collectors.partitioningBy(soundInfo -> ((AudioSoundZoneInfo)soundInfo).getAudioClipWritingResult().isSuccess(), Collectors.counting()));
+            return new SuccessResponse(soundZonesGenerationResult.get(true), soundZonesGenerationResult.get(false));
+        } catch (IOException e) {
+            errorResponse = new ErrorResponse(new BadRequestException(e.getMessage()));
+        } catch (BadRequestException | InternalServerErrorException e) {
+            errorResponse = new ErrorResponse(e);
         } catch (Exception e) {
-            System.out.println(new InternalServerErrorException(e));
+            errorResponse = new ErrorResponse(new InternalServerErrorException(e));
         }
-        // TODO get the consolidated result from AudioFileInfo through streams
-        return null;
+        return errorResponse;
     }
 
-    protected void validateAudioFileLocationInfo(AudioFileLocation audioFileLocation) {
+    private void validateAudioFileLocationInfo(AudioFileLocation audioFileLocation) {
         Path audioFileNamePath = Paths.get(audioFileLocation.getAudioFileName());
         Path outputAudioClipsPath = Paths.get(audioFileLocation.getOutputAudioClipsDirectoryPath());
 
@@ -72,7 +65,7 @@ public abstract class AudioSplitter {
         if (!Files.exists(outputAudioClipsPath)) throw AudioFileLocationException.outputDirectoryDoesNotExist(outputAudioClipsPath);
 
         Path audioFilePath = Paths.get(FilenameUtils.getPath(audioFileLocation.getAudioFileName()));
-        if (!audioFilePath.equals(outputAudioClipsPath)) throw AudioFileLocationException.sameAudioFileAndOutputDirectoryLocation();
+        if (audioFilePath.equals(outputAudioClipsPath)) throw AudioFileLocationException.sameAudioFileAndOutputDirectoryLocation();
     }
 
 }
