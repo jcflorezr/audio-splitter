a27d9f501d5eaf5de840e9a6a0af213710782015
Juan Camilo Flórez <jcflorezr@gmail.com>
2017-04-05 16:12:13 -0500

    [audio-splitter][testing] starting to create unit tests for AudioFileInfoServiceImpl (AudioConverterService)

--- src/main/java/net/jcflorezr/audiofileinfo/AudioFileInfoServiceImpl.java
+++ src/main/java/net/jcflorezr/audiofileinfo/AudioFileInfoServiceImpl.java
@@ -16,24 +16,19 @@ public class AudioFileInfoServiceImpl implements AudioFileInfoService {
     @Override
     public AudioFileInfo generateAudioFileInfo(AudioFileLocation audioFileLocation, boolean grouped) throws Exception {
         AudioFileInfo audioFileInfo = new AudioFileInfo(audioFileLocation);
-        try {
-            String convertedAudioFileName = audioConverterService.convertFileToWavIfNeeded(audioFileLocation.getAudioFileName());
-            audioFileInfo.setConvertedAudioFileName(convertedAudioFileName);
-            AudioContent audioContent = audioContentService.retrieveAudioContent(audioFileInfo, audioFileLocation);
-            audioFileInfo.setAudioContent(audioContent);
+        String convertedAudioFileName = audioConverterService.convertFileToWavIfNeeded(audioFileLocation.getAudioFileName());
+        audioFileInfo.setConvertedAudioFileName(convertedAudioFileName);
+        AudioContent audioContent = audioContentService.retrieveAudioContent(audioFileInfo, audioFileLocation);
+        audioFileInfo.setAudioContent(audioContent);
 
-            SoundZonesDetector soundZonesDetector = new SoundZonesDetector(audioContent.getOriginalAudioSignal());
-            audioFileInfo.setSingleAudioSoundZones(soundZonesDetector.getAudioSoundZones());
+        SoundZonesDetector soundZonesDetector = new SoundZonesDetector(audioContent.getOriginalAudioSignal());
+        audioFileInfo.setSingleAudioSoundZones(soundZonesDetector.getAudioSoundZones());
 
-            if (grouped) {
-                List<GroupAudioClipInfo> groupedAudioFileSoundZones =
-                        soundZonesDetector.retrieveGroupedAudioSoundZones(audioFileInfo.getSingleAudioSoundZones());
-                audioFileInfo.setGroupedAudioFileSoundZones(groupedAudioFileSoundZones);
-            }
-
-            return audioFileInfo;
-        } finally {
-
+        if (grouped) {
+            List<GroupAudioClipInfo> groupedAudioFileSoundZones =
+                    soundZonesDetector.retrieveGroupedAudioSoundZones(audioFileInfo.getSingleAudioSoundZones());
+            audioFileInfo.setGroupedAudioFileSoundZones(groupedAudioFileSoundZones);
         }
+        return audioFileInfo;
     }
 }
--- src/main/test/net/jcflorezr/audiofileinfo/AudioConverterServiceTest.java
+++ src/main/test/net/jcflorezr/audiofileinfo/AudioConverterServiceTest.java
@@ -1,5 +1,26 @@
-import static org.junit.Assert.*;
+package net.jcflorezr.audiofileinfo;
 
+import com.google.common.jimfs.Configuration;
+import com.google.common.jimfs.Jimfs;
+import net.jcflorezr.util.AudioUtils;
+import org.apache.commons.io.FilenameUtils;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({FilenameUtils.class, AudioUtils.class})
 public class AudioConverterServiceTest {
+
+    @Test
+    public void shouldConvertFileToWav() {
+
+    }
+
+    private void createFile(String audioFileName) {
+
+    }
+
 
 }
