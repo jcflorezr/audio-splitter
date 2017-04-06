e52d32a6988ae35fd6fb2cde0907cc5a520affda
Juan Camilo Flórez <jcflorezr@gmail.com>
2017-04-05 20:30:51 -0500

    [audio-splitter][testing] adding unit tests for AudioConverterService

--- src/main/test/net/jcflorezr/audiofileinfo/AudioConverterServiceTest.java
+++ src/main/test/net/jcflorezr/audiofileinfo/AudioConverterServiceTest.java
@@ -1,25 +1,65 @@
 package net.jcflorezr.audiofileinfo;
 
-import com.google.common.jimfs.Configuration;
-import com.google.common.jimfs.Jimfs;
+import net.jcflorezr.exceptions.InternalServerErrorException;
 import net.jcflorezr.util.AudioUtils;
-import org.apache.commons.io.FilenameUtils;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
+import javax.sound.sampled.UnsupportedAudioFileException;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.anyString;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
+import static org.powermock.api.mockito.PowerMockito.when;
+
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({FilenameUtils.class, AudioUtils.class})
+@PrepareForTest({AudioUtils.class})
 public class AudioConverterServiceTest {
 
-    @Test
-    public void shouldConvertFileToWav() {
+    private AudioConverterService audioConverterService;
 
+    @Before
+    public void setUp() {
+        audioConverterService = new AudioConverterService();
     }
 
-    private void createFile(String audioFileName) {
+    @Test
+    public void shouldConvertFileToWav() throws UnsupportedAudioFileException {
+        String audioFileName = "/any-path-to-file/any-file.mp3";
+        String convertedAudioFileName = "/any-path-to-file/any-file.wav";
+        mockStatic(AudioUtils.class);
+        when(AudioUtils.convertAudioFile(anyString(), anyString())).thenReturn(true);
+        assertEquals(convertedAudioFileName, audioConverterService.convertFileToWavIfNeeded(audioFileName));
+    }
 
+    @Test
+    public void audioFileFormatIsAlreadyWav() throws UnsupportedAudioFileException {
+        String audioFileName = "/any-path-to-file/any-file.wav";
+        String convertedAudioFileName = "/any-path-to-file/any-file.wav";
+        mockStatic(AudioUtils.class);
+        when(AudioUtils.convertAudioFile(anyString(), anyString())).thenReturn(true);
+        assertEquals(convertedAudioFileName, audioConverterService.convertFileToWavIfNeeded(audioFileName));
+    }
+
+    @Test(expected = UnsupportedAudioFileException.class)
+    public void audioFileCouldNotBeConverted() throws UnsupportedAudioFileException {
+        String audioFileName = "/any-path-to-file/any-file.wav";
+        String convertedAudioFileName = "/any-path-to-file/any-file.wav";
+        mockStatic(AudioUtils.class);
+        when(AudioUtils.convertAudioFile(anyString(), anyString())).thenReturn(false);
+        assertEquals(convertedAudioFileName, audioConverterService.convertFileToWavIfNeeded(audioFileName));
+    }
+
+    @Test(expected = InternalServerErrorException.class)
+    public void errorWhenConvertingFile() throws UnsupportedAudioFileException {
+        String audioFileName = "/any-path-to-file/any-file.wav";
+        String convertedAudioFileName = "/any-path-to-file/any-file.wav";
+        mockStatic(AudioUtils.class);
+        when(AudioUtils.convertAudioFile(anyString(), anyString())).thenThrow(new InternalServerErrorException(new Exception()));
+        assertEquals(convertedAudioFileName, audioConverterService.convertFileToWavIfNeeded(audioFileName));
     }
 
 
