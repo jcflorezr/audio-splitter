45a4cb351aea5982946702fcf4ff9e9071b562ca
Juan Camilo Flórez <jcflorezr@gmail.com>
2017-08-30 11:53:21 -0500

    [audio-splitter][persistence] creating mapper classes for Audio-Splitter database schema

--- src/main/java/net/jcflorezr/model/persistence/AudioClipsResult.java
+++ src/main/java/net/jcflorezr/model/persistence/AudioClipsResult.java
@@ -1,4 +1,105 @@
 package net.jcflorezr.model.persistence;
 
+import org.springframework.cassandra.core.PrimaryKeyType;
+import org.springframework.data.cassandra.mapping.PrimaryKeyColumn;
+import org.springframework.data.cassandra.mapping.Table;
+
+@Table(value = "audio_clip_result")
 public class AudioClipsResult {
+
+    @PrimaryKeyColumn(name = "audio_file_name", ordinal = 0, type = PrimaryKeyType.PARTITIONED)
+    private String audioFileName;
+    @PrimaryKeyColumn(name = "hours", ordinal = 1, type = PrimaryKeyType.CLUSTERED)
+    private int hours;
+    @PrimaryKeyColumn(name = "minutes", ordinal = 2, type = PrimaryKeyType.CLUSTERED)
+    private int minutes;
+    @PrimaryKeyColumn(name = "seconds", ordinal = 3, type = PrimaryKeyType.CLUSTERED)
+    private int seconds;
+    @PrimaryKeyColumn(name = "milliseconds", ordinal = 4, type = PrimaryKeyType.CLUSTERED)
+    private int milliseconds;
+
+    private boolean success;
+    private String errorMessage;
+
+    public String getAudioFileName() {
+        return audioFileName;
+    }
+
+    public void setAudioFileName(String audioFileName) {
+        this.audioFileName = audioFileName;
+    }
+
+    public int getHours() {
+        return hours;
+    }
+
+    public void setHours(int hours) {
+        this.hours = hours;
+    }
+
+    public int getMinutes() {
+        return minutes;
+    }
+
+    public void setMinutes(int minutes) {
+        this.minutes = minutes;
+    }
+
+    public int getSeconds() {
+        return seconds;
+    }
+
+    public void setSeconds(int seconds) {
+        this.seconds = seconds;
+    }
+
+    public int getMilliseconds() {
+        return milliseconds;
+    }
+
+    public void setMilliseconds(int milliseconds) {
+        this.milliseconds = milliseconds;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        AudioClipsResult that = (AudioClipsResult) o;
+
+        if (hours != that.hours) return false;
+        if (minutes != that.minutes) return false;
+        if (seconds != that.seconds) return false;
+        if (milliseconds != that.milliseconds) return false;
+        if (success != that.success) return false;
+        return audioFileName.equals(that.audioFileName);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = audioFileName.hashCode();
+        result = 31 * result + hours;
+        result = 31 * result + minutes;
+        result = 31 * result + seconds;
+        result = 31 * result + milliseconds;
+        result = 31 * result + (success ? 1 : 0);
+        return result;
+    }
 }
--- src/main/java/net/jcflorezr/model/persistence/AudioFileMetadata.java
+++ src/main/java/net/jcflorezr/model/persistence/AudioFileMetadata.java
@@ -1,4 +1,98 @@
 package net.jcflorezr.model.persistence;
 
+import org.springframework.data.cassandra.mapping.PrimaryKey;
+import org.springframework.data.cassandra.mapping.Table;
+
+import java.util.List;
+
+@Table(value = "audio_file_metadata")
 public class AudioFileMetadata {
+
+    @PrimaryKey("audio_file_name")
+    private String audioFileName;
+
+    private String title;
+    private String album;
+    private String artist;
+    private int trackNumber;
+    private String genre;
+    private List<String> rawMetadata;
+
+    public String getAudioFileName() {
+        return audioFileName;
+    }
+
+    public void setAudioFileName(String audioFileName) {
+        this.audioFileName = audioFileName;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAlbum() {
+        return album;
+    }
+
+    public void setAlbum(String album) {
+        this.album = album;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public int getTrackNumber() {
+        return trackNumber;
+    }
+
+    public void setTrackNumber(int trackNumber) {
+        this.trackNumber = trackNumber;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public List<String> getRawMetadata() {
+        return rawMetadata;
+    }
+
+    public void setRawMetadata(List<String> rawMetadata) {
+        this.rawMetadata = rawMetadata;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        AudioFileMetadata that = (AudioFileMetadata) o;
+
+        if (!audioFileName.equals(that.audioFileName)) return false;
+        if (!title.equals(that.title)) return false;
+        if (album != null ? !album.equals(that.album) : that.album != null) return false;
+        return artist != null ? artist.equals(that.artist) : that.artist == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = audioFileName.hashCode();
+        result = 31 * result + title.hashCode();
+        result = 31 * result + (album != null ? album.hashCode() : 0);
+        result = 31 * result + (artist != null ? artist.hashCode() : 0);
+        return result;
+    }
 }
